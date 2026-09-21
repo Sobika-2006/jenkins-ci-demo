3ded1a81f74cc4a4a5e594ac9b7b3fdd1a23adf2
Sobika <msobiga01012000@gmail.com>
2026-09-21 14:42:38 +0530

    Update HelloWorld message

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI Pipeline!");
+        System.out.println("Hello from Jenkins CI Pipeline - Build Updated!");
     }
 }
